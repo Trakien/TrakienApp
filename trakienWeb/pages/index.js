106617dd234c89abd4ba7102ff6402b71e468c9e
Sebastian Piñeros <sebas679pb>
2022-10-25 20:18:22 -0500

    home page init

--- trakienWeb/pages/index.js
+++ trakienWeb/pages/index.js
@@ -1,11 +1,37 @@
-export default function Login() {
+import React from "react";
+// Sections
+import TopNavbar from "../components/Nav/TopNavbar";
+import Header from "../components/Sections/Header";
+import Services from "../components/Sections/Services";
+import Projects from "../components/Sections/Projects";
+import Blog from "../components/Sections/Blog";
+import Pricing from "../components/Sections/Pricing";
+import Contact from "../components/Sections/Contact";
+import Footer from "../components/Sections/Footer"
+import {Helmet} from "react-helmet";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./style/flexboxgrid.min.css";
+import './style/index.css';
 
-  return (
-    <div className="container">
-      <title>Trakien</title>
-      <main>
-        <h1 className="title">Trakien</h1>
-      </main>
-    </div>
+export default function App() {
+  return (<><Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap" rel="stylesheet" />
+      </Helmet>
+    <>
+      <TopNavbar />
+      <Header />
+      <Services />
+      <Projects />
+      <Blog />
+      <Pricing />
+      <Contact />
+      <Footer />
+    </>
+    </>
   );
 }
+
+
