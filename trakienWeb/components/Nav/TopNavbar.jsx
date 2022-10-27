daba9877c34f004246a63b0ffc9aaeaaa69281cc
Sebastian Piñeros <sebas679pb>
2022-10-26 22:43:30 -0500

    hotfix signup redirect

--- trakienWeb/components/Nav/TopNavbar.jsx
+++ trakienWeb/components/Nav/TopNavbar.jsx
@@ -62,7 +62,7 @@ export default function TopNavbar() {
               </a>
             </li>
             <li className="semiBold font15 pointer flexCenter">
-              <a href="/" className="radius8 lightBg" style={{ padding: "10px 15px" }}>
+              <a href="/signup" className="radius8 lightBg" style={{ padding: "10px 15px" }}>
                 Sign up
               </a>
             </li>
